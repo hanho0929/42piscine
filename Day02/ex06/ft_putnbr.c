a965cc0e3c37e7ce063b111946ce4daa139a2bf3
Chenhan Ho <cheho@e1z1r6p11.42.us.org>
2018-08-23 18:23:59 -0700

    Day02

--- Day02/ex06/ft_putnbr.c
+++ Day02/ex06/ft_putnbr.c
@@ -6,26 +6,52 @@
 /*   By: cheho <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/23 14:44:56 by cheho             #+#    #+#             */
-/*   Updated: 2018/08/23 14:56:31 by cheho            ###   ########.fr       */
+/*   Updated: 2018/08/23 18:17:54 by cheho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
 
-void	ft_putchar(char c)
+void	ft_print(int num)
 {
-	write(1, &c, 1);
+	ft_putchar(num + '0');
 }
 
-void ft_putnbr(int nb)
+int		ft_pow(int i)
 {
-	int i;
+	int target;
 
-	i = nb;
-	ft_putchar(i + '0');
+	target = 1;
+	while (i > 0)
+	{
+		target *= 10;
+		i--;
+	}
+	return (target);
 }
 
-int main()
+void	ft_putnbr(int nb)
 {
-	ft_putnbr(20);
-	return(0);
+	int temp;
+	int count;
+	int cur;
+
+	count = 0;
+	cur = 0;
+	if (nb < 0)
+	{
+		nb = -nb;
+	}
+	temp = nb;
+	while (temp >= 10)
+	{
+		temp /= 10;
+		count++;
+	}
+	while (count > 0)
+	{
+		cur = nb / ft_pow(count);
+		ft_putchar(cur + '0');
+		nb = nb - cur * ft_pow(count);
+		count--;
+	}
+	ft_putchar(nb + '0');
 }
