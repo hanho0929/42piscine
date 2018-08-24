d625cc277f7f99183a60b078654cb0a371b5f877
Chenhan Ho <cheho@e1z1r6p11.42.us.org>
2018-08-23 20:19:02 -0700

    add

--- Day02/ex07/ft_print_combn.c
+++ Day02/ex07/ft_print_combn.c
@@ -6,13 +6,61 @@
 /*   By: cheho <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/23 15:25:53 by cheho             #+#    #+#             */
-/*   Updated: 2018/08/23 15:46:49 by cheho            ###   ########.fr       */
+/*   Updated: 2018/08/23 20:14:07 by cheho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_print_combn(int n)
+void	ft_find(int *arr, int col, int n)
 {
-	int number[n];
+	int i;
 
+	while (col)
+	{
+		while (arr[col] == (9 + col - (n - 1)) && col >= 0)
+			col -= 1;
+		if (col < 0)
+			break ;
+		arr[col] += 1;
+		i = col;
+		while (i++ < n)
+		{
+			arr[i] = arr[i - 1] + 1;
+		}
+		col = n - 1;
+		ft_putchar(',');
+		ft_putchar(' ');
+		i = 0;
+		while (i < n)
+			ft_putchar(arr[i++] + '0');
+	}
+}
 
+void	ft_print_combn(int n)
+{
+	int arr[n];
+	int i;
+	int col;
+
+	col = n - 1;
+	if (n > 10)
+		return ;
+	i = 0;
+	while (i < n)
+	{
+		arr[i] = i;
+		i++;
+	}
+	i = 0;
+	while (i < n)
+	{
+		ft_putchar(arr[i] + '0');
+		i++;
+	}
+	ft_find(arr, col, n);
+}
+
+int		main(void)
+{
+	ft_print_combn(3);
+	return (0);
 }
